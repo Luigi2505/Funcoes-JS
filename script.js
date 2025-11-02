6b721894f13b6000cf519740953424d602680a4b
Luigi Bilyk de Oliveira <bilyk.luigi@gmail.com>
2025-11-02 20:55:44 -0300

    funcoes

---
+++ script.js
@@ -0,0 +1,18 @@
+const out = document.getElementById("saida");
+
+function soma(a, b) {
+  return a + b;
+}
+
+function subtracao(a, b) {
+  return a - b;
+}
+
+function multiplicacao(a, b) {
+  return a * b;
+}
+
+function divisao(a, b) {
+  if (b === 0) return "Erro: divisão por zero!";
+  return a / b;
+}
