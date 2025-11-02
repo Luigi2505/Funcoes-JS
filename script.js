d015b51e356e70263202f87b66116bb2cb98a1a7
Luigi Bilyk de Oliveira <bilyk.luigi@gmail.com>
2025-11-02 20:56:12 -0300

    filtro

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ function divisao(a, b) {
   if (b === 0) return "Erro: divisão por zero!";
   return a / b;
 }
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const pares = numeros.filter(function (n) {
+  return n % 2 === 0;
+});
+
+function calcularMedia(arr) {
+  let soma = arr.reduce(function (acumulador, numero) {
+    return acumulador + numero;
+  }, 0);
+
+  return soma / arr.length;
+}
+
+const media = calcularMedia(numeros);
+
+function executarOperacao(a, b, operacaoCallback) {
+  return operacaoCallback(a, b);
+}
